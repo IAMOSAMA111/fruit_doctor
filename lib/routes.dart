9ff5ad8c4855f12091f47dfbc9114c2f8639b5a7
MalikSaqib141947 <37288377+MalikSaqib141947@users.noreply.github.com>
2020-11-12 22:09:38 +0500

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -4,15 +4,18 @@ import 'Screens/signup.dart';
 import 'Screens/bottomnav.dart';
 import 'Screens/home.dart';
 import 'Screens/weather.dart';
+import 'Screens/welcome.dart';
+import 'Screens/forgotPassword.dart';
+
 class Routes {
- static Map<String, WidgetBuilder> _defaultRoute={
-
-   Login.id: (context) => Login(),
-   Signup.id: (context) => Signup(),
-   Home.id: (context) => Home(),
-   Weather.id: (context) => Weather(),
-   BottomNavigation.id : (context) => BottomNavigation(),
-
- };
- static Map<String, WidgetBuilder> get ROUTE => _defaultRoute;
+  static Map<String, WidgetBuilder> _defaultRoute = {
+    Welcome.id: (context) => Welcome(),
+    Forgot_Password.id: (context) => Forgot_Password(),
+    Login.id: (context) => Login(),
+    Signup.id: (context) => Signup(),
+    Home.id: (context) => Home(),
+    Weather.id: (context) => Weather(),
+    BottomNavigation.id: (context) => BottomNavigation(),
+  };
+  static Map<String, WidgetBuilder> get ROUTE => _defaultRoute;
 }
