24470c8caca9c0622bdb7cd924c0612b8dec24c0
MalikSaqib141947 <37288377+MalikSaqib141947@users.noreply.github.com>
2020-11-12 22:09:41 +0500

    Update bottomnav.dart

--- lib/Screens/bottomnav.dart
+++ lib/Screens/bottomnav.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'home.dart';
+import 'package:flutter_doctor/Screens/home.dart';
+import 'package:flutter_doctor/utilities/constants.dart';
+
 class BottomNavigation extends StatefulWidget {
-  static const String id ='BottomNavigation'  ;
+  static const String id = 'BottomNavigation';
   @override
   _BottomNavigationState createState() => _BottomNavigationState();
 }
 
 class _BottomNavigationState extends State<BottomNavigation> {
-  int _selectedPage =0;
+  int _selectedPage = 0;
   final _pageOption = [
     Home(),
-    Text("Community", style: TextStyle(fontSize: 20),),
-    Text("Todoist", style: TextStyle(fontSize: 20),),
-    Text("Profile", style: TextStyle(fontSize: 20),),
+    Text(
+      "Community",
+      style: TextStyle(fontSize: 20),
+    ),
+    Text(
+      "Todoist",
+      style: TextStyle(fontSize: 20),
+    ),
+    Text(
+      "Profile",
+      style: TextStyle(fontSize: 20),
+    )
   ];
 
   @override
@@ -21,41 +32,39 @@ class _BottomNavigationState extends State<BottomNavigation> {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-          body:_pageOption[_selectedPage],
-            bottomNavigationBar: BottomNavigationBar(
-              selectedItemColor:  Color(0xff45736A),
-              unselectedItemColor: Colors.blueGrey,
-              currentIndex: _selectedPage,
-              onTap: (int index){
-                setState(() {
-                  _selectedPage = index;
-                });
-              },
-              items: [
-
-                BottomNavigationBarItem(
-
-                  icon:FaIcon(FontAwesomeIcons.home,),
-                  title: Text("Home"),
+          body: _pageOption[_selectedPage],
+          bottomNavigationBar: BottomNavigationBar(
+            selectedItemColor: primary_Color,
+            unselectedItemColor: Colors.blueGrey,
+            currentIndex: _selectedPage,
+            onTap: (int index) {
+              setState(() {
+                _selectedPage = index;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: FaIcon(
+                  FontAwesomeIcons.home,
                 ),
-                BottomNavigationBarItem(
-                  icon:FaIcon(FontAwesomeIcons.comment),
-                  title: Text("Community"),
-                ),
-                BottomNavigationBarItem(
-                  icon:FaIcon(FontAwesomeIcons.stickyNote),
-                  title: Text("Todoist"),
-                ),
-                BottomNavigationBarItem(
-                  icon:FaIcon(FontAwesomeIcons.user),
-                  title: Text("Profile"),
-                )
-
-              ],
-            ),
+                title: Text("Home"),
+              ),
+              BottomNavigationBarItem(
+                icon: FaIcon(FontAwesomeIcons.comment),
+                title: Text("Community"),
+              ),
+              BottomNavigationBarItem(
+                icon: FaIcon(FontAwesomeIcons.stickyNote),
+                title: Text("Todoist"),
+              ),
+              BottomNavigationBarItem(
+                icon: FaIcon(FontAwesomeIcons.user),
+                title: Text("Profile"),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
