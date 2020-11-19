e6c501170514132802476593a70e0f30d2c84c07
MalikSaqib141947 <37288377+MalikSaqib141947@users.noreply.github.com>
2020-11-19 19:22:10 +0500

    Update auth.dart

--- lib/utilities/auth.dart
+++ lib/utilities/auth.dart
@@ -3,16 +3,31 @@ library fruit_doctor.auth;
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as JSON;
 import 'package:flutter_doctor/Screens/bottomnav.dart';
 
-Auth a = new Auth();
+_Auth a = _Auth.getInstance();
 
-class Auth {
+enum E { username, email, photoURL }
+
+class _Auth {
+  static _Auth _instance = new _Auth();
   bool isLoggedIn = false;
-  Map userProfile;
+  List<String> userProfile = List<String>(3);
+  Map userPrf;
   static final facebookLogin = FacebookLogin();
+  GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+
+  _Auth() {}
+
+  static _Auth getInstance() {
+    if (_instance == null) {
+      _instance = new _Auth();
+    }
+    return _instance;
+  }
 
   loginWithFB(BuildContext context) async {
     final result = await facebookLogin.logInWithReadPermissions(['email']);
@@ -26,7 +41,12 @@ class Auth {
         final profile = JSON.jsonDecode(graphResponse.body);
         takeToHome(context);
         print(profile);
-        userProfile = profile;
+        userPrf = profile;
+        //setting the user data fields
+        userProfile[E.username.index] = userPrf['name'];
+        userProfile[E.email.index] = userPrf['email'];
+        userProfile[E.photoURL.index] = userPrf['picture']['data']['url'];
+
         isLoggedIn = true;
         break;
 
@@ -41,10 +61,33 @@ class Auth {
 
   logout() async {
     facebookLogin.logOut();
+    _googleSignIn.signOut();
     isLoggedIn = false;
-    userProfile = null;
+    //userProfile = null;
     print('logged out');
   }
+
+  loginWithGoogle(BuildContext context) async {
+    try {
+      await _googleSignIn.signIn();
+      userProfile[E.username.index] = _googleSignIn.currentUser.displayName;
+      userProfile[E.email.index] = _googleSignIn.currentUser.email;
+      userProfile[E.photoURL.index] = _googleSignIn.currentUser.photoUrl;
+      takeToHome(context);
+      isLoggedIn = true;
+    } catch (err) {
+      print(err);
+    }
+  }
+
+/*
+  _logout(){
+    _googleSignIn.signOut();
+    setState(() {
+      _isLoggedIn = false;
+    });
+  }
+  */
 
   takeToHome(BuildContext context) {
     Navigator.push(
