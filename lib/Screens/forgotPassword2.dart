753649c9d2f000800fcb91d0402f2ff30cdfe684
MalikSaqib141947 <37288377+MalikSaqib141947@users.noreply.github.com>
2020-12-19 13:52:08 +0500

    Update forgotPassword2.dart

--- lib/Screens/forgotPassword2.dart
+++ lib/Screens/forgotPassword2.dart
@@ -2,139 +2,159 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_doctor/Screens/forgotPassword3.dart';
+import 'package:flutter_doctor/Screens/loader.dart';
 import 'package:flutter_doctor/utilities/constants.dart';
+import 'package:flutter_doctor/utilities/auth.dart' as auth;
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_doctor/Screens/login.dart';
 import 'package:animated_widgets/animated_widgets.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:pin_entry_text_field/pin_entry_text_field.dart';
+import 'package:flutter_gifimage/flutter_gifimage.dart';
 
-class Forgot_Password2 extends StatefulWidget {
-  static const String id = 'Forgot_Password2';
-
+class ForgotPassword2 extends StatefulWidget {
+  static const String id = 'EnterPin';
+  var _email;
+  ForgotPassword2(e) {
+    this._email = e;
+  }
   @override
-  _Forgot_Password2State createState() => _Forgot_Password2State();
+  _ForgotPassword2State createState() => _ForgotPassword2State(_email);
 }
 
-class _Forgot_Password2State extends State<Forgot_Password2> {
-  String code;
+class _ForgotPassword2State extends State<ForgotPassword2> {
+  var _email;
+  bool isLoading = false;
 
+  _ForgotPassword2State(e) {
+    this._email = e;
+    sendMail();
+  }
+/////
+  sendMail() {
+    auth.a.sendMail(_email, context).then((val) {
+      if (val.data['success']) {
+        Fluttertoast.showToast(
+            msg: val.data['msg'],
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            backgroundColor: Colors.green,
+            textColor: Colors.white,
+            fontSize: 16.0);
+      } else {
+        Fluttertoast.showToast(
+            msg: val.data['msg'],
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+      }
+    });
+  }
+
+/////
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return SafeArea(
-        child: Scaffold(
-      body: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Column(children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SvgPicture.asset('assets/images/Components/upper-left.svg',
-                    height: size.height * 0.1),
-                SizedBox(
-                  width: 55,
-                ),
-                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-                  SizedBox(height: 50),
-                  Text(
-                    'FORGOT PASSWORD',
-                    style: topHeadingStyle,
-                  ),
-                ]),
-                SizedBox(
-                  width: 50,
-                ),
-                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-                  SizedBox(height: 50),
-                  RotationAnimatedWidget.tween(
-                    duration: Duration(minutes: 3),
-                    delay: Duration(milliseconds: 500),
-                    enabled: true,
-                    rotationDisabled: Rotation.deg(z: -3600),
-                    rotationEnabled: Rotation.deg(z: 0),
-                    child: SvgPicture.asset(
-                        'assets/images/Components/upper-right.svg',
-                        height: size.height * 0.04),
-                  )
-                ]),
-                SizedBox(height: 120),
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(height: 30),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset(
-                        'assets/images/Components/forgot-password.svg',
-                        height: size.height * 0.27),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Text(
-                  "Don't Worry, You can easily reset it!",
-                  style: labelStyle1,
-                ),
-                SizedBox(height: 40),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  decoration: loginBoxDecorationStyle,
-                  height: 50.0,
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  child: TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    textAlignVertical: TextAlignVertical.center,
-                    onChanged: (value) {
-                      this.code = value;
-                    },
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'CM Sans Serif',
-                    ),
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      prefixIcon: Icon(
-                        Icons.alternate_email,
-                        color: placeholderColor,
-                      ),
-                      hintText: 'The code sent to your email',
-                      hintStyle: placeholderStyle,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                ButtonTheme(
-                  buttonColor: primary_Color,
-                  minWidth: size.width * 0.8,
-                  height: size.height * 0.05,
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30)),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => (Forgot_Password3())));
-                    },
-                    child: Text(
-                      'Continue',
-                      style: TextStyle(color: Colors.white, fontSize: 15),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 80),
-                Row(
-                  children: [
-                    SvgPicture.asset('assets/images/Components/bottom-left.svg',
-                        height: size.height * 0.15),
-                  ],
-                )
-              ],
-            )
-          ])),
-    ));
+    Size socialLogin = MediaQuery.of(context).size;
+    Color c = Colors.blue;
+
+    if (isLoading) {
+      return Loader();
+    } else
+      return SafeArea(
+          child: Scaffold(
+              body: Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: Stack(children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 100),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(400),
+                          child: Image(
+                            height: 200,
+                            image: AssetImage(
+                                "assets/images/email_sent_animation3.gif"),
+                          ),
+                        ),
+                        SizedBox(height: 50),
+                        Text("Enter the 6 digit pin sent to your email!",
+                            style: TextStyle(
+                              color: primary_Color,
+                            )),
+                        SizedBox(height: 50),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: 10,
+                            ),
+                            PinEntryTextField(
+                              fields: 6,
+                              showFieldAsBox: true,
+                              onSubmit: (String pin) {
+                                if (pin.toString() == auth.a.code.toString()) {
+                                  Fluttertoast.showToast(
+                                      msg: "Verification Successful",
+                                      toastLength: Toast.LENGTH_SHORT,
+                                      gravity: ToastGravity.BOTTOM,
+                                      backgroundColor: Colors.green,
+                                      textColor: Colors.white,
+                                      fontSize: 16.0);
+                                  //taking the user to change password page
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              (Forgot_Password3(_email))));
+                                } else {
+                                  Fluttertoast.showToast(
+                                      msg: "Verification Failed",
+                                      toastLength: Toast.LENGTH_SHORT,
+                                      gravity: ToastGravity.BOTTOM,
+                                      backgroundColor: Colors.red,
+                                      textColor: Colors.white,
+                                      fontSize: 16.0);
+                                }
+                              },
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 50),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Did not receive the code? ",
+                              style: labelStyle1,
+                            ),
+                            GestureDetector(
+                              onTap: () => {
+                                auth.a.sendMail(_email, context).then((val) {
+                                  if (val.data['success']) {
+                                    Fluttertoast.showToast(
+                                        msg: val.data['msg'],
+                                        toastLength: Toast.LENGTH_SHORT,
+                                        gravity: ToastGravity.BOTTOM,
+                                        backgroundColor: Colors.green,
+                                        textColor: Colors.white,
+                                        fontSize: 16.0);
+                                  }
+                                })
+                              },
+                              child: Text("Resend Email",
+                                  style: TextStyle(
+                                      color: primary_Color,
+                                      fontWeight: FontWeight.bold)),
+                            )
+                          ],
+                        ),
+                      ],
+                    )
+                  ]))));
   }
 }
